20bd9e057d42ffb6e60f610f4d0c422878cc54cc
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-07-10 16:55:51 +0200

    feat: milestone 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Article from "./components/Article";
 
 
 function App() {
+
+  const [addedProducts, setAddedProducts] = useState([]);
 
   const products = [
     { name: 'Mela', price: 0.5 },
@@ -10,11 +13,29 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  function addToCart(product) {
+    const productInCart = addedProducts.find(p => p.name === product.name);
+    if (!productInCart) {
+      setAddedProducts([...addedProducts, { ...product, quantity: 1 }])
+    }
+  }
+
   return (
     <>
 
-      {products && products.map((p, i) => <Article key={i} data={p} />)}
-
+      {products && products.map((p, i) =>
+        <div key={i}>
+          <Article data={p} />
+          <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
+        </div>)}
+      {addedProducts.length > 0 && (
+        <>
+          <h3>Carrello</h3>
+          {addedProducts.map((p, i) => <div key={i}>
+            <span>{p.name} quantità: {p.quantity}</span>
+          </div>)}
+        </>
+      )}
     </>
   )
 }
